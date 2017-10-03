9b9085231c770c7fa061283daf8d1902ebe77349
Eric Bolinger <ericb@ericbisme.net>
2017-10-02 19:07:42 -0600

    add service and populate manifests

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,4 +7,11 @@
 # @example
 #   include nginx::install
 class nginx::install {
+  String $ensure       = $nginx::ensure,
+  String $package_name = $nginx::package_name,
+) {
+  package { 'install_nginx': 
+    ensure => $ensure,
+    name   => $package_name,
+  }
 }
